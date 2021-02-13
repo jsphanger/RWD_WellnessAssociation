ace3ef42df84c7f80ac1ddbc622b51175ed47194
Joseph Anger <jsphanger@yahoo.com>
2021-02-13 17:13:49 -0600

    Image Preloader
    - For the large view we now pre-load the background images for smaller backgrounds in order to have a smoother responsive feel.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,6 +1,21 @@
+var images = [];
 const icon = document.getElementById("mobileNavHamburger");
 const tray = document.getElementById("menuItems")
 
+//Preload images for a smoother responsive experience
+function preload(){
+    if(window.innerWidth > 1295)
+    {
+        for(var i = 0; i < arguments.length; i++){
+            images[i] = new Image();
+            images[i].src = preload.arguments[i];
+        }
+    }
+}
+
+preload("Images/Background.jpg", "Images/BackgroundTablet.jpg", "Images/BackgroundPhone.jpg");
+
+//Handle menu clicks
 icon.onclick = function(){
     tray.classList.contains("navTrayVisible") ? tray.classList.remove("navTrayVisible") : tray.classList.add("navTrayVisible");
 };
